7c40b265b0dc014975c002b7fe66837066a6d8f8
selesdepselesnul <selesdepselesnul@gmail.com>
2016-05-24 05:07:05 +0700

    init

---
+++ src/jca/Main.java
@@ -0,0 +1,25 @@
+package jca;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        AnchorPane root = fxmlLoader.load(ClassLoader.getSystemResourceAsStream("jca/main.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/jca/MainController.java
@@ -0,0 +1,126 @@
+package jca;
+
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainController {
+
+    @FXML
+    private TextArea chatTextArea;
+
+    @FXML
+    private Button listenButton;
+
+    @FXML
+    private CheckBox serverCheckBox;
+
+    @FXML
+    private TextField portTextField;
+
+    @FXML
+    private TextField destPortTextField;
+
+    @FXML
+    private TextField destIPTextField;
+
+    @FXML
+    private TextArea messageTextArea;
+
+    ServerSocket serverSocket;
+    Socket socket;
+
+    public void handleServerCheckBox(ActionEvent actionEvent) {
+        if (serverCheckBox.isSelected())
+            listenButton.setText("listen");
+        else
+            listenButton.setText("connect");
+    }
+
+    @FXML
+    public void handleClickListenButton(ActionEvent e) throws IOException {
+
+        String currentText = listenButton.getText();
+        if(serverCheckBox.isSelected()) {
+            if (currentText.equals("listen")) {
+                System.out.println("listen");
+
+                int port = Integer.parseInt(portTextField.getText());
+                System.out.println(port);
+                serverSocket = new ServerSocket(port);
+                Task<Void> task = new Task<Void>() {
+                    @Override
+                    protected Void call() throws Exception {
+
+                        Socket clientSocket;
+                        while ((clientSocket = serverSocket.accept()) != null) {
+                            BufferedReader buff = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                            chatTextArea.setText(buff.readLine());
+//                            System.out.println(buff.read());
+                            buff.close();
+                        }
+
+                        return null;
+                    }
+                };
+                new Thread(task).start();
+                listenButton.setText("unlisten");
+            } else {
+                serverSocket.close();
+                listenButton.setText("listen");
+            }
+        } else {
+            if (currentText.equals("connect")) {
+                int port = Integer.parseInt(destPortTextField.getText());
+                String ip = destIPTextField.getText();
+                System.out.println(port);
+                System.out.println(ip);
+                Task<Void> task = new Task<Void>() {
+                    @Override
+                    protected Void call() throws Exception {
+                        try {
+                            socket = new Socket(ip, port);
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                        }
+                        return null;
+                    }
+                };
+                new Thread(task).start();
+
+                listenButton.setText("disconnect");
+            } else {
+                socket.close();
+                listenButton.setText("connect");
+            }
+        }
+    }
+
+    @FXML
+    public void handleSendButton() throws IOException {
+        if(serverCheckBox.isSelected()) {
+
+        } else {
+            int port = Integer.parseInt(destPortTextField.getText());
+            String ip = destIPTextField.getText();
+            socket = new Socket(ip, port);
+            socket.setTcpNoDelay(true);
+            System.out.println("send");
+            BufferedWriter buff = new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream()));
+            buff.write(messageTextArea.getText());
+            buff.flush();
+            buff.close();
+            socket.close();
+        }
+    }
+
+}
