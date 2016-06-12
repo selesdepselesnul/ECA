5d6d6a7b435abd0d662b36989dd3bfd83d928f5f
selesdepselesnul <selesdepselesnul@gmail.com>
2016-06-13 06:58:55 +0700

    add feature to make font smaller or bigger

--- src/eca/MainController.java
+++ src/eca/MainController.java
@@ -25,6 +25,7 @@ public class MainController implements Initializable {
     private ChatManager chatManager;
     private Connection connection = new Connection();
     private Thread systemTextThread;
+    private Task<Void> messageTask;
 
     private String[] splitKeyValue(String str) {
         final String[] keyValArr = str.split("=");
@@ -60,7 +61,8 @@ public class MainController implements Initializable {
     private void giveIntro() {
         chatTextArea.setText(
                 "F1 if you know what do u mean ?\nF2 to stop it \n"
-                        +"F3 f*@king boring intro\nF4 back to this message\nF5 exec command\nF6 exit");
+                        +"F3 f*@king boring intro\nF4 back to this message\nF5 exec command\n"
+                        +"F6 make font smaller\nF7 make font bigger\nF8 exit");
     }
 
 
@@ -72,15 +74,14 @@ public class MainController implements Initializable {
                 chatTextArea::setText
         );
 
-        chatTextArea.setStyle("-fx-background-color: black;");
         giveIntro();
         chatTextArea.setOnKeyReleased(e -> {
             try {
                 if(connection.isConnect)
                     chatManager.send();
+                if(messageTask ==  null || !messageTask.isRunning()) {
+                    if(e.getCode() == KeyCode.F1) {
 
-                if(e.getCode() == KeyCode.F1) {
-                    if(systemTextThread ==  null) {
                         Map<String, String> connectionMap = parse(chatTextArea.getText());
                         if(connectionMap != null) {
                             if(connectionMap.containsKey("mode") && connectionMap.size() >=2) {
@@ -108,46 +109,50 @@ public class MainController implements Initializable {
 
                         System.out.println(connection);
                         checkModeAndStatus();
-                    }
-                }  else if(e.getCode() == KeyCode.F2) {
-                    if(!systemTextThread.isAlive() && connection.isConnect) {
-                        if(connection.mode.equals("client")) {
-                            System.out.println("close client");
-                            chatManager.close();
-                            animateText("client connection close!");
-                            connection.isConnect = false;
-                        } else {
-                            System.out.println("close server");
-                            if(!serverSocket.isClosed())
-                                serverSocket.close();
-                            chatManager.close();
-                            animateText("server connection close!");
-                            connection.isConnect = false;
+
+                    }  else if(e.getCode() == KeyCode.F2) {
+                        if(!systemTextThread.isAlive() && connection.isConnect) {
+                            if(connection.mode.equals("client")) {
+                                System.out.println("close client");
+                                chatManager.close();
+                                animateText("client connection close!");
+                                connection.isConnect = false;
+                            } else {
+                                System.out.println("close server");
+                                if(!serverSocket.isClosed())
+                                    serverSocket.close();
+                                chatManager.close();
+                                animateText("server connection close!");
+                                connection.isConnect = false;
+                            }
                         }
-                    }
-                } else if(e.getCode() == KeyCode.F3) {
-                    try(BufferedReader buff = new BufferedReader(new InputStreamReader(
-                            ClassLoader.getSystemResourceAsStream("eca/message.txt")));) {
-                        animateText(buff.lines().collect(Collectors.joining("\n")));
-                    } catch (IOException ioE) {
-                        ioE.printStackTrace();
-                    } finally {
-                        systemTextThread = null;
-                    }
-                } else if(e.getCode() == KeyCode.F4) {
-                    giveIntro();
-                } else if(e.getCode() == KeyCode.F5) {
-                    Runtime runtime = Runtime.getRuntime();
-                    Process process = runtime.exec(chatTextArea.getSelectedText());
-                    try(BufferedReader buff = new BufferedReader(new InputStreamReader(
-                            process.getInputStream()));) {
-                        chatTextArea.appendText("\noutput:\n"+buff.lines().collect(Collectors.joining("\n")));
-                    } catch (IOException ioE) {
-                        ioE.printStackTrace();
+                    } else if(e.getCode() == KeyCode.F3) {
+                        try(BufferedReader buff = new BufferedReader(new InputStreamReader(
+                                ClassLoader.getSystemResourceAsStream("eca/message.txt")));) {
+                            animateText(buff.lines().collect(Collectors.joining("\n")));
+                        } catch (IOException ioE) {
+                            ioE.printStackTrace();
+                        }
+                    } else if(e.getCode() == KeyCode.F4) {
+                        giveIntro();
+                    } else if(e.getCode() == KeyCode.F5) {
+                        Runtime runtime = Runtime.getRuntime();
+                        Process process = runtime.exec(chatTextArea.getSelectedText());
+                        try(BufferedReader buff = new BufferedReader(new InputStreamReader(
+                                process.getInputStream()));) {
+                            chatTextArea.appendText("\noutput:\n"+buff.lines().collect(Collectors.joining("\n")));
+                        } catch (IOException ioE) {
+                            ioE.printStackTrace();
+                        }
+
+                    } else if(e.getCode() == KeyCode.F6) {
+                        chatTextArea.setStyle("-fx-font-size : " + (chatTextArea.getFont().getSize() - 1));
+                    } else if(e.getCode() == KeyCode.F7) {
+                        chatTextArea.setStyle("-fx-font-size : " + (chatTextArea.getFont().getSize() + 1));
+                    } else if(e.getCode() == KeyCode.F8) {
+                        Platform.exit();
                     }
 
-                } else if(e.getCode() == KeyCode.F6) {
-                    Platform.exit();
                 }
 
 
@@ -162,7 +167,7 @@ public class MainController implements Initializable {
     private void animateText(String text) {
         this.chatTextArea.clear();
         this.chatTextArea.setEditable(false);
-        Task<Void> task = new Task<Void>() {
+        messageTask = new Task<Void>() {
             @Override
             protected Void call() throws Exception {
                 for (int i=0; i<text.length();i++) {
@@ -177,7 +182,7 @@ public class MainController implements Initializable {
                 return null;
             }
         };
-        systemTextThread = new Thread(task);
+        systemTextThread = new Thread(messageTask);
         systemTextThread.start();
 
     }
@@ -210,8 +215,7 @@ public class MainController implements Initializable {
                         }
                     });
                     animateText(
-                            "Client connect to server, "
-                            +"type some word to your friend over there!");
+                            "Client connect to the server");
                     connection.isConnect = true;
                 }
             }
